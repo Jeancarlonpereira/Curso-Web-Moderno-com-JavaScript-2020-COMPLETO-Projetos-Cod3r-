93d2570f830acb82551ce0a4d160d78cf1b1bfeb
Jean Pereira <jeancarlo.npereira@gmail.com>
2020-09-12 11:06:32 -0300

    finalizando fundamentos JS

---
+++ fundamentos/aritimeticos.js
@@ -0,0 +1,11 @@
+const [a,b,c,d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(modulo)
