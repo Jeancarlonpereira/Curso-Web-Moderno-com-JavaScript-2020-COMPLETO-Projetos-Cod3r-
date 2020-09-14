43435d8566205d7f1de01acd5c01a92c78955381
Jean Pereira <jeancarlo.npereira@gmail.com>
2020-09-13 22:41:31 -0300

    objetos

---
+++ objeto/jsonVSObj.js
@@ -0,0 +1,9 @@
+const { json } = require("body-parser")
+
+const obj = {a: 1, b: 2, c: 3, soma(){return a + b + c} }
+console.log(JSON.stringify(obj))
+
+// console.log(JSON.parse("{a: 1, b:2, c:3}"))
+// console.log(JSON.parse("{'a': 1, 'b':2, 'c':3}"))
+console.log(JSON.parse('{"a": 1, "b":2, "c":3}'))
+console.log(JSON.parse('{"a": 1, "b":"string", "c":true, "d": {}, "e": []}'))
