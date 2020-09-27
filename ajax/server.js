af4038d77dc1b824414a6dcb0d60ea2b73930cdc
Jean Pereira <jeancarlo.npereira@gmail.com>
2020-09-26 22:09:34 -0300

    xmlHTTP parte 1

--- ajax/server.js
+++ ajax/server.js
@@ -7,11 +7,32 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+const multer = require('multer')
 
-app.get('./teste', (req,res)=>{
-    res.send("ok")
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
 })
 
+
+const upload = multer({storage}).single('arquivo')
+
+// app.get('/teste', (req,res)=>{
+//     res.send("ok")
+// })
+
+app.post('/upload', (req, resp)=>{
+    upload(req, resp, err => {
+        if(err) {
+            return resp.end('Ocorreu um erro.')
+        }
+        resp.end('Concluido com sucesso.')
+    })
+})
 app.listen(3000, () => {
     console.log('Executando.....')
 })
